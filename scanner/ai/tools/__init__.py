52f346a5e5ed51217d3dd741763b766beeff98d7
Pepe Simon <ppipotsch@gmail.com>
2026-01-04 10:49:29 +0100

    actionable project plan

--- scanner/ai/tools/__init__.py
+++ scanner/ai/tools/__init__.py
@@ -1,3 +1,3 @@
 from .credential_search import search_web, submit_credentials
-from .credential_testing import make_credential_testing_tools, submit_credentials_validity
+from .credential_testing import make_credential_testing_tools
 from .url_fetching import fetch_url_summary, fetch_url
