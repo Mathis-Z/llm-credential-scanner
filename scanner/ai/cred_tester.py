c678730a675dad7f3b1884a4f8c70e7e872df383
Pepe Simon <ppipotsch@gmail.com>
2026-01-25 21:58:56 +0100

    Remove Simhash detection

--- scanner/ai/cred_tester.py
+++ scanner/ai/cred_tester.py
@@ -126,8 +126,8 @@ class CredTester(DBConnectionMixin, Thread):
             driver.quit()
 
         login_successful = (
-            (before_path != after_path) or
-            (simhash.Simhash(before_page_source).distance(simhash.Simhash(after_page_source)) > 8)
+            (before_path != after_path) #or
+            #(simhash.Simhash(before_page_source).distance(simhash.Simhash(after_page_source)) > 8)
         )
         logger.debug("Login %s: before_path=%s after_path=%s simhash_distance=%s",
                      "successful" if login_successful else "failed",
