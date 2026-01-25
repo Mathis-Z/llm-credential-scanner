35f7aa68fdbef9727a4f1f70c644b70608be4997
Pepe Simon <ppipotsch@gmail.com>
2026-01-25 23:51:03 +0100

    increase docker compose timeout due to large apps

--- scanner/tests/startup_script.py
+++ scanner/tests/startup_script.py
@@ -71,7 +71,7 @@ class StartupScript:
 class RunDockerCompose(StartupScript):
     """Run a docker compose file from the test-network directory and wait for a port to respond."""
 
-    def __init__(self, compose_file_path: str, wait_for_port, timeout: int = 300):
+    def __init__(self, compose_file_path: str, wait_for_port, timeout: int = 600):
         full_path = Path(__file__).parent / "test-network" / compose_file_path
         compose_file_name = full_path.name if (full_path.suffix == ".yaml" or full_path.suffix == ".yml") else "docker-compose.yml"
 
